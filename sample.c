f5d8479860c9a8af6f8984f94f5d3a486d1e4919
root <root@ip-172-31-17-45.us-west-2.compute.internal>
2017-10-12 19:24:24 +0000

    html file is created

--- sample.c
+++ sample.c
@@ -3,3 +3,16 @@ C Programming is an ANSI/ISO standard and powerful programming language for deve
 
 This C programming tutorial is designed for the new learners, students and also for the corporate level developers who want to learn and refresh their C programming skills.
 
+START C PROGRAMMING TUTORIAL
+
+C PROGRAMMING HISTORY:
+
+The C language is a structure oriented programming language, was developed at Bell Laboratories in 1972 by Dennis Ritchie. C language features were derived from an earlier language called “B” (Basic Combined Programming Language – BCPL). C language was invented for implementing UNIX operating system….more…
+
+C PROGRAMMING BASICS:
+
+Steps to be followed for any C program to create and get the output are explained . This is common to all C programs and there is no exception, whether its a very small C program or very large program. Structure of a C program is defined by a set of rules called protocol, to be followed by programmer while writing a C program…more…
+
+C – PRINTF AND SCANF:
+
+printf() and scanf() functions are inbuilt library functions in C which are available in the C library by default. These functions are declared and related macros are defined in “stdio.h” which is a header file. We have to include “stdio.h” file as shown in below C program to make use of these printf() and scanf() library functions….more…
