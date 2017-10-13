4a166e97d12f18cd94dfd1f15691f9bd0ba74511
root <root@ip-172-31-17-45.us-west-2.compute.internal>
2017-10-13 16:58:44 +0000

    new code added sample.c

--- sample.c
+++ sample.c
@@ -1,4 +1,37 @@
- programming tutorial
+#include <stdio.h>
+ 
+main()
+{
+   int value = 1;
+ 
+   while(value<=3)
+   {
+      printf("Value is %d\n", value);
+      value++;
+   }
+ 
+   return 0;
+}
+Output:
+Value is 1
+Value is 2
+Value is 3 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+programming tutorial
 C Programming is an ANSI/ISO standard and powerful programming language for developing real time applications. C programming language was invented by Dennis Ritchie at the Bell Laboratories in 1972. It was invented for implementing UNIX operating system. C is most widely used programming language even today. All other programming languages were derived directly or indirectly from C programming concepts. This tutorial explains all basic concepts in C like history of C language, data types, keywords, constants, variables, operators, expressions, control statements, array, pointer, string, library functions, structures and unions etc.
 
 This C programming tutorial is designed for the new learners, students and also for the corporate level developers who want to learn and refresh their C programming skills.
